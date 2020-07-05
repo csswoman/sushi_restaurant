c73849450080d2df53aef52ea171624af6c4da2b
Karla Agraz <karla.agraz@gmail.com>
2020-07-05 17:31:43 -0500

    Animaciones CSS para el slider

--- assets/javascript/slider/sliderDOM.js
+++ assets/javascript/slider/sliderDOM.js
@@ -4,29 +4,55 @@ let sliderText = document.querySelector("#slider-text");
 let sliderTitle = document.querySelector("#slider-title");
 let sliderSubTitle = document.querySelector("#slider-subtitle");
 let sliderImage = document.querySelector("#slider-image");
+let textContent = document.querySelector("#slider-text-content")
 
 let slider = new Slider ({
     elements: [
         {
             title: 'Lorem Ipsum',
             subtitle: 'Impum',
-            image: '../public/images/8.jpeg',
+            image: '../public/images/12.jpg',
             text: 'Lorem Ipsum fedrfgvr grfgvb fgrd'
         },
         {
             title: 'Lorem Ipsum 2',
             subtitle: 'Impum 2',
-            image: '../public/images/9.jpeg',
+            image: '../public/images/14.jpg',
+            text: 'Lorem Ipsum fedrfgvr grfgvb fgrd'
+        },
+        {
+            title: 'Lorem Ipsum 3',
+            subtitle: 'Impum 2',
+            image: '../public/images/15.jpg',
             text: 'Lorem Ipsum fedrfgvr grfgvb fgrd'
         }
     ],
     animationFunc: function(element){
-        sliderTitle.innerHTML = element.title;
-        sliderSubTitle.innerHTML = element.subtitle;
-        sliderText.innerHTML = element.text;
-        sliderImage.src = element.image;
-},
-    speed: 2000
-});
 
-slider.play();
+        textContent.classList.add("hide");
+        sliderImage.classList.add("hide");
+    
+        setTimeout(function(){
+          sliderTitle.innerHTML = element.title;
+          sliderSubTitle.innerHTML = element.subtitle;
+          sliderText.innerHTML = element.text;
+          sliderImage.src = element.image;
+    
+          textContent.classList.remove("hide");
+          sliderImage.classList.remove("hide");
+        },600);
+    
+      },
+      speed: 5000
+    });
+    
+    slider.play();
+    
+
+    
+
+
+        
+    
+
+
