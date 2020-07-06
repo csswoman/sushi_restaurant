0985b1614069750421b61a94a4dc0297f0b963d3
Karla Agraz <karla.agraz@gmail.com>
2020-07-06 01:44:05 -0500

    js

---
+++ assets/javascript/slider/elements.js
@@ -0,0 +1,22 @@
+const elements = [
+    {
+        title: 'Lorem ipsum dolor',
+        subtitle: 'Impum',
+        image: '../public/images/12.jpg',
+        text: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, placeat.'
+    },
+    {
+        title: 'Lorem Ipsum 2',
+        subtitle: 'Impum 2',
+        image: '../public/images/14.jpg',
+        text: 'Lorem Ipsum fedrfgvr grfgvb fgrd'
+    },
+    {
+        title: 'Lorem Ipsum 3',
+        subtitle: 'Impum 2',
+        image: '../public/images/15.jpg',
+        text: 'Lorem Ipsum fedrfgvr grfgvb fgrd'
+    }
+];
+
+export default elements;
