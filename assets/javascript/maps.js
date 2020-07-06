feb6103dafa49ec8944a1c7aa485476409046872
Karla Agraz <karla.agraz@gmail.com>
2020-07-06 02:12:24 -0500

    añadiendo marcador

--- assets/javascript/maps.js
+++ assets/javascript/maps.js
@@ -1,10 +1,19 @@
 function initMap() {
+
+    const coords = {
+        lat: 34.047490,
+        lng: -118.239440
+    };
+
     let map = new google.maps.Map(document.getElementById('mapa'), {
-        center: {
-            lat: 34.047490,
-            lng: -118.239440
-        },
+        center: coords,
         zoom:16,
+    });
+
+    let marker = new google.maps.Marker({
+        position: coords,
+        map,
+        title: 'Sushi Bar'
     })
 }
 
