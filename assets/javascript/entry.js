00268f37c07e42f5668f4602bde8e7c42d8f7461
Karla Agraz <karla.agraz@gmail.com>
2020-07-07 14:49:30 -0500

    hamburger menu

--- assets/javascript/entry.js
+++ assets/javascript/entry.js
@@ -3,3 +3,14 @@ import '../css/app.scss';
 import './slider/sliderDOM.js';
 
 import './maps';
+
+document.querySelector(".menu")
+    .addEventListener("click", function(){
+        document.querySelector(".menu-screen").classList.add("active");
+    });
+
+
+document.querySelector(".close")
+    .addEventListener("click", function(){
+        document.querySelector(".menu-screen").classList.remove("active");
+    });
