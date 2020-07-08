a749e33c6d4ca55e0af76cd67e229cf8d2e7282a
Karla Agraz <karla.agraz@gmail.com>
2020-07-07 19:29:54 -0500

    Animar transiciones del menú

--- assets/javascript/entry.js
+++ assets/javascript/entry.js
@@ -4,13 +4,4 @@ import './slider/sliderDOM.js';
 
 import './maps';
 
-document.querySelector(".menu")
-    .addEventListener("click", function(){
-        document.querySelector(".menu-screen").classList.add("active");
-    });
-
-
-document.querySelector(".close")
-    .addEventListener("click", function(){
-        document.querySelector(".menu-screen").classList.remove("active");
-    });
+import './menu';
