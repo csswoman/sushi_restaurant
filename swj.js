c6d6d41112c820284fd86d101c7a62501d2dce65
Karla Agraz <karla.agraz@gmail.com>
2020-07-09 18:22:30 -0500

    service workers

---
+++ swj.js
@@ -0,0 +1,8 @@
+const CACHE_NAME = 'STORIES_CACHE-v1';
+
+self.addEventListener('install', function(){
+    
+    caches.open(CACHE_NAME).then(function(cache){
+        cache.addAll(['/index.html']);
+    })
+});
