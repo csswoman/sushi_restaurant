86b298ddddd3f559767ae892ce401cc99748d0dd
Karla Agraz <karla.agraz@gmail.com>
2020-07-10 16:41:08 -0500

    Extraer estilos en un archivo css

---
+++ webpack/webpack.prod.js
@@ -0,0 +1,28 @@
+const common = require('./webpack.common.js');
+const merge = require('webpack-merge');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+    filename: 'dist/css[name].[contenthash].css'
+})
+
+module.exports = merge(common, {
+    output:{
+        publicPath: '.'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [
+                        {loader: 'css-loader', options: {minimize: true}},
+                        {loader: 'sass-loader'}
+                    ]
+                })
+            }
+        ]
+    },
+    plugins: [extractSass]
+});
